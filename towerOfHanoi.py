b6dad7238d7f5dab0f0f6dcf2ca0f36fd3237dd7
Nadeem Kabha <nadeem.kabha@gmail.com>
2023-03-27 04:00:07 +0300

    edit for python

--- towerOfHanoi.py
+++ towerOfHanoi.py
@@ -2,7 +2,6 @@ def towerOfHanoi(size:int, fromr:str, help:str, to:str):
 
     # size: num of disks, fromr: the current rod,to: the destination rod, help: the remaining rod
     if (size == 1):
-    
         print(f"Move a disk from {fromr} to {to}" )
         return
     
@@ -11,17 +10,19 @@ def towerOfHanoi(size:int, fromr:str, help:str, to:str):
     towerOfHanoi(size - 1, help, fromr, to)
 
 
+def main():
+    size = 1
+    while (size):
 
-size = 1
+        size=int(input("Please enter the desired number of disks (0 for exit): "))
 
-while (size):
+        if (not size):
+            print("End!")
+        elif (size < 0):
+            print("Wrong input please try again!")
+        else:
+            towerOfHanoi(size, 'A', 'B', 'C')
 
-    size=int(input("Please enter the desired number of disks (0 for exit): "))
 
-    if (not size):
-        print("End!")
-    elif (size < 0):
-        print("Wrong input please try again!")
-    else:
-        towerOfHanoi(size, 'A', 'B', 'C')
-
+if __name__ == "__main__":
+    main()
